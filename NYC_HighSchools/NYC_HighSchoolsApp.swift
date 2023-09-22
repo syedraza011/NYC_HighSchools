4b6d5b9a0dec0d695f0f086bfb656ecd3152112f
Syed Raza <syed.raza011@gmail.com>
2023-09-22 04:22:29 -0400

    Initial Commit

---
+++ NYC_HighSchools/NYC_HighSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYC_HighSchoolsApp.swift
+//  NYC_HighSchools
+//
+//  Created by Syed Raza on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NYC_HighSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
